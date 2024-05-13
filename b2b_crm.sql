1b49a4e79c968901fc862d6021c2dbf9ea313a66
Nguyen Trung Nghia <156154939+NghiaNT3110@users.noreply.github.com>
2024-05-13 20:39:09 +0700

    Update b2b_crm.sql

--- b2b_crm.sql
+++ b2b_crm.sql
@@ -7,7 +7,7 @@ LEFT JOIN totemic-vine-391706.maven_b2b_sale_case.products p ON p.product = sp.p
 LEFT JOIN totemic-vine-391706.maven_b2b_sale_case.sales_teams t ON t.string_field_0 = sp.sales_agent
 )
 SELECT s.*, a.sector, a.revenue,
-a.revenue / s.sales_price AS quantity,
+s.close_value / s.sales_price AS quantity,
 a.office_location, a.year_established, 
 a.employees AS company_size
 FROM sale s
